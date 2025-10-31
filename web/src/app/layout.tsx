1c8dcac2e2e47955db00d4d726f1231ddf0ccace
glownest2026-droid <glownest2026@gmail.com>
2025-10-31 07:21:31 +0000

    fix: remove @apply; plain CSS helpers

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,6 +1,5 @@
 // web/src/app/layout.tsx
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "../components/Header";
 
